4b168507b21640efc3ba5afea9904d0d2d2a0ae6
Aditya kumar <128294354+adityakumar2003@users.noreply.github.com>
2026-09-25 09:24:14 +0530

    Update  final U_reg.php

--- U_reg.php
+++ U_reg.php
@@ -183,6 +183,30 @@
 
 </body>
 
+
+<?php
+session_start();
+require "db.php";
+
+$username = $_POST['username'];
+$password = $_POST['"password'];
+
+$stmt = $pdo->prepare("SELECT * FROM users WHERE username = ?");
+$stmt->execute([$username]);
+
+$user = $stmt->fetch();
+
+if ($user && password_verify($password, $user['password'])) {
+    session_regenerate_id(true);
+
+    $_SESSION['user_id'] = $user['id'];
+    $_SESSION['username'] = $user['username'];
+
+    echo "Login successful";
+} else {
+    echo "Invalid username or password";
+}
+
 </html>
 
 <?php
